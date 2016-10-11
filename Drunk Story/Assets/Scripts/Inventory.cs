b7532f98112f11e758504299f2d3f6663951e78a
sportguitarRC <raf87crosby@hotmail.com>
2016-10-11 19:45:03 -0400

    fail de rafa

---
+++ Drunk Story/Assets/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+    private int nbOfCan;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public int GetNbOfCan()
+    {
+        return nbOfCan;
+    }
+
+    public void IncreaseObjectAmount(TypeObject typeObject)
+    {
+        switch (typeObject)
+        {
+            case TypeObject.BeerCan:
+                nbOfCan--;
+                break;
+        }
+    }
+
+    public void DecreaseObjectAmount(TypeObject typeObject)
+    {
+        switch (typeObject)
+        {
+            case TypeObject.BeerCan:
+                nbOfCan--;
+                break;
+        }
+    }
+}
---
+++ Drunk Story/Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TypeObject
+{
+    BeerCan
+};
+
+public class InventoryObject : MonoBehaviour {
+
+    public TypeObject typeObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("AWAILLE SPORTGUITAR");
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Inventory>().IncreaseObjectAmount(typeObject);
+            Destroy(gameObject);
+        }
+    }
+}
