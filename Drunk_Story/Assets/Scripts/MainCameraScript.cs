f1e330d148d5cca63f7fa86af87061b0ecd25192
Emile Fortin <sylfschiffer@gmail.com>
2016-10-28 14:15:09 -0400

    Ajout d'une gestion intelligente de la caméra

---
+++ Drunk_Story/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraScript : MonoBehaviour {
+
+	public GameObject[] players;
+	private int numberOfPlayers;
+
+	// Use this for initialization
+	void Start () {
+        numberOfPlayers = players.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float avgX = 0;
+        foreach (GameObject g in players)
+            avgX += g.transform.position.x;
+        avgX /= numberOfPlayers;
+        float avgY = 0;
+        foreach (GameObject g in players)
+            avgY += g.transform.position.y;
+        avgY /= numberOfPlayers;
+
+        transform.position = new Vector3(avgX, avgY, -10f);
+    }
+}
