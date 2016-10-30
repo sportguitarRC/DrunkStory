375eb25a19dc965c19cbfb49bd27fcbf2588396b
Pierre-André Savard <pandre711@gmail.com>
2016-10-30 18:46:14 -0400

    Sprint 3
    Ajout du build des niveaux et correction des prefabs de joueurs

---
+++ Drunk_Story/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
