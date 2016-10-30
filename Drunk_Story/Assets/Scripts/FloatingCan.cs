d191089c71e7506f616c1e5298e53387ac733392
sportguitarRC <raf87crosby@hotmail.com>
2016-10-30 13:11:23 -0400

    floating cans are working

---
+++ Drunk_Story/Assets/Scripts/FloatingCan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FloatingMethod
+{
+    UpDown,
+    LeftRight,
+    WeirdRound
+};
+
+public class FloatingCan : MonoBehaviour {
+    private float varX = 0;
+    private float varY = 0.5f;
+
+    public FloatingMethod fMethod;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (fMethod == FloatingMethod.UpDown)
+            transform.Translate(new Vector3(0.0f, Mathf.Sin(varY) * 0.15f, 0f));
+        else if (fMethod == FloatingMethod.LeftRight)
+            transform.Translate(new Vector3(Mathf.Sin(varX) * 0.15f, 0.0f, 0f));
+        else
+            transform.Translate(new Vector3(Mathf.Sin(varX) * 0.15f, Mathf.Sin(varY) * 0.15f, 0f));
+
+        varX += 0.1f;
+        varY += 0.1f;
+	}
+}
