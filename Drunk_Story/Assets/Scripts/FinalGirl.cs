543fad8a6c014f899e9cbada6ee3ad5976b1ce48
Mathieu Guilmette <mathieuguilmette21@gmail.com>
2016-10-31 23:02:58 -0400

    Sprint 3 - Condition de Victoire - Mathieu
    Ajout de la FinalGirl + debut du script , pas terminé , je vais
    continuer tantot mais je push au cas ou quelquun dautre veut faire de
    quoi pendant mon absence !

---
+++ Drunk_Story/Assets/Scripts/FinalGirl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FinalGirl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject Winner = GameObject.FindWithTag("Player");
+            //MARCHE PAS MAIS JE PUSH AU CAS OU DAUTRE PERSONNES VOULAIT FAIRE DE QUOI
+            //SceneManager.LoadScene("End");
+        }
+    }
+}
