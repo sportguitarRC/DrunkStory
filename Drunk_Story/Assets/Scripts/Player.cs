874cfd4aa576548dc7460926d52a1678c2fa14e7
Mathieu Guilmette <mathieuguilmette21@gmail.com>
2016-10-31 21:17:13 -0400

    Sprint 4 - Add Audio - Mathieu
    Ajout du son du jump du player et du puke...still a lot of problems !

--- Drunk_Story/Assets/Scripts/Player.cs
+++ Drunk_Story/Assets/Scripts/Player.cs
@@ -1,58 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Controller2D))]
-public class Player : MonoBehaviour {
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
 
-	public float jumpHeight = 4;
-	public float timeToJumpApex = .4f;
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
     public string pNumber;
-	float accelerationTimeAirborne = .2f;
-	float accelerationTimeGrounded = .1f;
-	float moveSpeed = 6;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 6;
     Animator animator;
 
-	float gravity;
-	float jumpVelocity;
-	Vector3 velocity;
-	float velocityXSmoothing;
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
 
-	Controller2D controller;
+    Controller2D controller;
 
     float drunknessLevel;
 
-	void Start() {
-		controller = GetComponent<Controller2D> ();
-        animator = GetComponent<Animator>();
+    AudioSource[] sounds;
+    AudioSource jumpSound;
+    AudioSource pukeSound;
 
-        gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        animator = GetComponent<Animator>();
+        sounds = GetComponents<AudioSource>();
+
+        jumpSound = sounds[0];
+        pukeSound = sounds[1];
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 
         ResetDrunkness();
-	}
+    }
 
-	void Update() {
+    void Update()
+    {
 
-		if (controller.collisions.above || controller.collisions.below) {
-			velocity.y = 0;
-		}
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
 
-		Vector2 input = new Vector2 (Input.GetAxisRaw ( pNumber + "Horizontal"), Input.GetAxisRaw (pNumber + "Vertical"));
+        Vector2 input = new Vector2(Input.GetAxisRaw(pNumber + "Horizontal"), Input.GetAxisRaw(pNumber + "Vertical"));
 
-		if (Input.GetButtonDown (pNumber + "Jump") && controller.collisions.below) {
-			velocity.y = jumpVelocity;
-        
-		}
+        if (Input.GetButtonDown(pNumber + "Jump") && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        if (Input.GetButtonDown(pNumber + "Jump") && controller.collisions.below)
+        {
+            jumpSound.Play();
+        }
 
         if (drunknessLevel == 6)
+        {
             Puke();
+            pukeSound.Play();
+        }
 
-		float targetVelocityX = input.x * (moveSpeed - drunknessLevel/2) ;
-		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+        float targetVelocityX = input.x * (moveSpeed - drunknessLevel / 2);
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         animator.SetFloat("currentSpeed", input.x);
-		velocity.y += gravity * Time.deltaTime;
-		controller.Move (velocity * Time.deltaTime);
-	}
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 
     void ResetDrunkness()
     {
